0fd093369617fcfe7b9b539327b44c4280798b9f
masud-rana44 <mdmasudrana4488@gmail.com>
2023-12-07 23:29:00 +0600

    Implemented navbar

---
+++ src/app/(main-layout)/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/shared/Footer";
+import Navbar from "@/components/shared/Navbar";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default layout;
